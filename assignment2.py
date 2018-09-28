8aac0d06a150b124e5024ce0e239245f65a6ee23
Deepak Kaushal <deepakt754@gmail.com>
2018-09-28 02:26:06 -0700

    first commit

---
+++ assignment2.py
@@ -0,0 +1,40 @@
+# Q.1- Print anything you want on screen.
+print("Daku")
+
+#Q.2- Join two strings using '+'.
+d="Acad"+"View”
+print(d)
+
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+
+x=input("Enter value of x: ")
+y=input("Enter value of y: ")
+z=input("Enter value of z: ")
+print(x,y,z)
+
+#Q.4- Print “Let’s get started” on screen.
+print("Let's get started")
+
+#Q.5- Print the following values using placeholders.
+#s=”Acadview”
+#course=”Python”
+#fees=5000
+
+s="Acadview"
+course="Python"
+fees=5000
+print("%s,%s,%d"%(s,course,fees))
+
+#Q.6- Find the area of circle
+#pi = 3.14
+#Take radius as input from user
+#Print the area of circle
+pi=3.14
+r=float(input("Enter the radius: "))
+print(pi*r*r)
+print(pi*r**2)
+
+
+
+
+
