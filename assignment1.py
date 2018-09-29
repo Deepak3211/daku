736a9b011845613355675af1c26bc1b3d893b0eb
Deepak Kaushal <deepakt754@gmail.com>
2018-09-28 20:01:06 -0700

    first commit

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+git init
+git config --global user.name "username"
+git config --global user.email "email"
+git add test.txt
+git commit -m "first commit"
+git remote add origin https://github.com/Deepak3211/daku1.git
+git push -u origin master
+git status
+git add.
+git log
+
+
