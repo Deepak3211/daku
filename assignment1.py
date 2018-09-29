dda3881b26412d07170021e6b2fb9fc705f0b94c
Deepak3211 <43593743+Deepak3211@users.noreply.github.com>
2018-09-28 20:04:52 -0700

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
 git init
 git config --global user.name "username"
-git config --global user.email "email"
+git config --global user.email "user email"
 git add test.txt
 git commit -m "first commit"
 git remote add origin https://github.com/Deepak3211/daku1.git
