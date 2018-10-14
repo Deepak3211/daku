67091de33c2026c6cc19cc5de911a432cc43a2f8
Deepak3211 <43593743+Deepak3211@users.noreply.github.com>
2018-10-14 11:31:40 +0530

    Update ass_2.py

--- ass_2.py
+++ ass_2.py
@@ -1,4 +1,4 @@
-"""#Q.1- Create a user defined dictionary and get keys corresponding to the value using for loop. 
+#Q.1- Create a user defined dictionary and get keys corresponding to the value using for loop. 
 
 n1=int(input("enter total number of keys: "))
 d1={}
@@ -9,7 +9,7 @@ l=[]
 for j in d1:
     l.append(j)
 print("keys of dictionary is: ",l)
-"""
+
 #Q.2- Create a dictionary and store student names and create nested dictionary to store the subject wise marks of every student.Print the marks of a given student from that dictionary for every subject.
 
 daku={"daku1":{"maths":85,"science":86,"computer":67,"english":79},"daku2":{"maths":74,"english":67,"thermo":45,"science":76},"daku3":{"som":85,"maths":65,"ed":53,"electrical":78}}
